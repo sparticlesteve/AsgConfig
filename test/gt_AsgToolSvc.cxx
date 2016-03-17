3f8f9c0c5d905bda262d816f022bcaa4f3cfa26a
Steve Farrell <sfarrell@cern.ch>
2016-03-17 16:53:29 -0700

    moved AsgToolSvc test code into googletest

---
+++ test/gt_AsgToolSvc.cxx
@@ -0,0 +1,37 @@
+// Support
+#include "AsgTools/MessageCheck.h"
+#include "AsgTools/UnitTest.h"
+#include "gtest/gtest.h"
+#ifdef ROOTCORE
+#  include "xAODRootAccess/Init.h"
+#endif
+
+// Local includes
+#include "AsgConfig/AsgToolSvc.h"
+
+// STL
+#include <string>
+
+namespace
+{
+  using namespace asg::msgUserCode;
+
+  TEST(ToolSvcTest, CreateTool)
+  {
+    ana::AsgToolSvc toolSvc("AsgToolSvc");
+    const std::string toolName = "MuonSelectionToolTest";
+    const std::string toolType = "CP::MuonSelectionTool";
+    auto tool = toolSvc.getTool(toolName, toolType);
+    EXPECT_TRUE( tool != nullptr );
+  }
+}
+
+int main(int argc, char** argv)
+{
+#ifdef ROOTCORE
+  StatusCode::enableFailure();
+  ANA_CHECK( xAOD::Init() );
+#endif
+  ::testing::InitGoogleTest(&argc, argv);
+  return RUN_ALL_TESTS();
+}
