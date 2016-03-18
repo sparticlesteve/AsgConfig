e67b05f0d8b99638d5f70220342b8bde187b106f
Steve Farrell <sfarrell@cern.ch>
2016-03-18 14:39:12 -0700

    UTs for ConfigSvc

---
+++ test/gt_ConfigSvc.cxx
@@ -0,0 +1,41 @@
+#include "AsgTools/MessageCheck.h"
+#include "AsgTools/UnitTest.h"
+
+#include "CxxUtils/make_unique.h"
+
+#include "gtest/gtest.h"
+
+#include "AsgConfig/ConfigSvc.h"
+#include "AsgConfig/ChildTool.h"
+
+namespace
+{
+  using namespace ana;
+
+  TEST(ConfigSvcTest, EmptyProperties)
+  {
+    auto& configSvc = ConfigSvc::getInstance();
+    const auto& propList = configSvc.getProperties("test");
+    EXPECT_EQ( propList.size(), 0u );
+  }
+
+  TEST(ConfigSvcTest, ConfigureTool)
+  {
+    auto& configSvc = ConfigSvc::getInstance();
+    const std::string toolName = "TestTool";
+    ana::FloatProperty ptProp("MuonPtMin", 20.f);
+    ana::StringProperty stringProp("StringProp", "testString");
+    ana::BoolProperty boolProp("BoolProp", true);
+    configSvc.setProperty(toolName, &ptProp);
+    configSvc.setProperty(toolName, &stringProp);
+    configSvc.setProperty(toolName, &boolProp);
+    ChildTool tool(toolName);
+    EXPECT_SUCCESS( configSvc.configureTool(&tool) );
+  }
+}
+
+int main(int argc, char** argv)
+{
+  ::testing::InitGoogleTest( &argc, argv );
+  return RUN_ALL_TESTS();
+}
